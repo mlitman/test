9f49bd915b7fccb38942413c9ccd867107ca9e45
Mike Litman <awesomefat@gmail.com>
2015-08-27 10:57:32 -0500

    made another change

--- ChartTest3/AppDelegate.swift
+++ ChartTest3/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 //made change
     
+    //another change
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         return true
